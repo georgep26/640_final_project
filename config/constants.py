e8b73908e5d760aed20d1c2211d91d035d827417
George Padavick <george.padavick@gmail.com>
2021-12-04 14:44:32 -0500

    Added constants file with mapping for Q3 labels

---
+++ config/constants.py
@@ -0,0 +1,12 @@
+theme_labels = {
+    1: "Gun/2nd Amendment Rights",
+    2: "Gun Control/Regulation",
+    3: "Politics",
+    4: "Mental Health",
+    5: "School or Public Space Safety",
+    6: "Race/Ethnicity",
+    7: "Public Opinion",
+    8: "Society/Culture",
+    9: "Economic Consequences",
+    99: "None of the above (No Label)",
+}
