46b52a1d2331dece87667c596f305ce7800cb9d2
George Padavick <george.padavick@gmail.com>
2021-12-05 20:55:43 -0500

    fixing error with config writer

--- config/config_writer.py
+++ config/config_writer.py
@@ -7,8 +7,11 @@ class ConfigWriter:
         self.config = {}
         self.out_string = ""
 
-    def add(self, title, dictionary):
-        self.config[title] = dictionary
+    def add(self, title, item):
+        if title in self.config.keys():
+            self.config[title] = self.config[title].extend(item)
+        else:
+            self.config[title] = item
 
     def print(self, print_string):
         self.out_string = self.out_string + "\n" + print_string
