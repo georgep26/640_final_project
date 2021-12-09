9b6d66d65d820f26967de327970314f2c6ba3d72
George Padavick <georgep4@scc1.bu.edu>
2021-12-09 08:10:12 -0500

    adding all models

---
+++ model_exploration/model_results/multimodal_20210712145205/multimodal_config.py
@@ -0,0 +1,33 @@
+from model_exploration.models.bert_model_HW5_baseline import BERTTextClassifierBase
+from model_exploration.models.bert_model_deep import BERTTextClassifierBaseDeep
+from model_exploration.models.multimodal import MultimodalClassifier
+
+import model_exploration.datasets.bert_text_encoding_dataset as text_dataset
+import model_exploration.datasets.multimodal_dataset as multimodal_dataset
+from torch import nn
+from transformers import BertModel, BertTokenizer, AdamW
+
+# Config for all BERT model runs
+
+bert_baseline_model = {
+    "model_name": "multimodal",
+    "model_class": MultimodalClassifier,
+    "optimizer": AdamW,
+    "loss_fn": nn.CrossEntropyLoss(),
+    "num_classes": 10,
+    "num_epochs": 15,
+    "dropout": 0.5,
+    "bert_model_path": "model_exploration/model_results/bert_baseline_hw5_25_epochs_20210612094657/best_model_state.bin"
+}
+
+bert_baseline_data = {
+    "train_data_loc": "data/preprocessed_data/data_train.csv",
+    "test_data_loc": "data/preprocessed_data/data_test.csv",
+    "dataset_type": multimodal_dataset,
+    "tokenizer": BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True),
+    "text_col": ["headline"],
+    "pred_col": "Q3 Theme1",
+    "num_workers": 4,
+    "max_len": 50,
+    "batch_size": 128,
+}
---
+++ model_exploration/model_results/x_bert_deep_headline_15_200_20210712233818/bert_config.py
@@ -0,0 +1,29 @@
+from model_exploration.models.bert_model_HW5_baseline import BERTTextClassifierBase
+from model_exploration.models.bert_model_deep import BERTTextClassifierBaseDeep
+import model_exploration.datasets.bert_text_encoding_dataset as text_dataset
+from torch import nn
+from transformers import BertModel, BertTokenizer, AdamW
+
+# Config for all BERT model runs
+
+bert_baseline_model = {
+    "model_name": "x_bert_deep_headline_15_200",
+    "model_class": BERTTextClassifierBaseDeep,
+    "optimizer": AdamW,
+    "loss_fn": nn.CrossEntropyLoss(),
+    "num_classes": 10,
+    "num_epochs": 15,
+    "dropout": 0.5
+}
+
+bert_baseline_data = {
+    "train_data_loc": "data/preprocessed_data/data_train.csv",
+    "test_data_loc": "data/preprocessed_data/data_test.csv",
+    "dataset_type": text_dataset,
+    "tokenizer": BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True),
+    "text_col": ["headline", "google_visual_api_web_entities_detection_on_lead_image"],
+    "pred_col": "Q3 Theme1",
+    "num_workers": 4,
+    "max_len": 200,
+    "batch_size": 16
+}
